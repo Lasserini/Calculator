d2de1016a18aa7ccac46328aed93c9bf7fd5335b
Lasserini <lasserini@gmail.com>
2022-05-02 06:20:40 +0000

    Add calculate functionality

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ NB. Its a work in progress, actual calculation not inplemented yet (1/5-2022)
 ### Ideas for future expansion
 - Add a way to display previous calculations.
 - Expand with more advanced math expressions.
+- Add support for float values.
--- assets/script.js
+++ assets/script.js
@@ -4,37 +4,63 @@ const operatorBtn = document.querySelectorAll('[data-operator]');
 const calculateBtn = document.querySelector('[data-calculate]');
 const deleteBtn = document.querySelector('[data-delete]');
 var numInputs = 0;
-var inputs = [];
+var inputs ="";
 
 numBtn.forEach(button => {
     button.addEventListener('click', () => {
         oldInput = document.getElementById("results").innerText;
         let updatedInput = oldInput + button.innerText;
         document.getElementById("results").innerHTML = updatedInput;
-        numInputs = numInputs + button.innerText;
+        //numInputs = numInputs + button.innerText;
+        inputs = inputs + button.innerText;
     })
 })
 
 operatorBtn.forEach(button => {
     button.addEventListener('click', () => {
-        inputs.push(parseInt(numInputs));
-        numInputs = 0;
+        // inputs.push(numInputs);
+        // numInputs = 0;
         oldInput = document.getElementById("results").innerText;
         let updatedInput = oldInput + button.innerText;
         document.getElementById("results").innerHTML = updatedInput;
-        inputs.push(button.innerHTML);
+        // inputs.push(button.innerHTML);
+        inputs = inputs + button.innerText;
     })
 })
 
 deleteBtn.addEventListener('click', () => {
     document.getElementById("results").innerHTML = "";
-    numInputs = 0;
-    inputs = [];
+    // numInputs = 0;
+    inputs = "";
 })
 
 calculateBtn.addEventListener('click', () => {
-    inputs.push(parseInt(numInputs));
-    numInputs = 0;
-    document.getElementById("results").innerHTML = inputs;
+    // inputs.push(numInputs);
+    // numInputs = 0;
+    calculation = inputs.match(/\d+|[^0-9]/g);
+    for (var a = 0; a < calculation.length; a++) {
+        if (a == 0) {
+          result = parseInt(calculation[a]);
+        } else {
+          if (!isNaN(calculation[a])) {
+            switch (calculation[a - 1]) {
+              case "-":
+                result -= parseInt(calculation[a]);
+                break;
+              case "+":
+                result += parseInt(calculation[a]);
+                break;
+              case "*":
+                result *= parseInt(calculation[a]);
+                break;
+              case "/":
+                result /= parseInt(calculation[a]);
+                break;
+            }
+          }
+        }
+    }
+    console.log(result);
+    document.getElementById("results").innerHTML = result;
     inputs = [];
 })
